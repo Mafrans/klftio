8f0c4de7bd603c54214287dc0807033bd492490a
Mafrans <malte.kluft@gmail.com>
2021-08-18 13:57:36 +0200

    🐛 Update model to fix bugs

--- util/model.ts
+++ util/model.ts
@@ -9,7 +9,7 @@ export type Project = {
     summary: string;
     description: string;
     image: string;
-    gallery: (string | GalleryItem)[];
+    gallery: GalleryItem[];
 }
 
 export const projects: Project[] = [
@@ -19,12 +19,12 @@ export const projects: Project[] = [
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum .',
         image: 'https://source.unsplash.com/random',
         gallery: [
-            'https://source.unsplash.com/random',
+            { image: 'https://source.unsplash.com/random' },
             { name: 'Title', image: 'https://source.unsplash.com/random'},
             { name: 'Title', summary: 'Summary', image: 'https://source.unsplash.com/random'},
-            'https://source.unsplash.com/random',
-            'https://source.unsplash.com/random',
-            'https://source.unsplash.com/random',
+            { image: 'https://source.unsplash.com/random' },
+            { image: 'https://source.unsplash.com/random' },
+            { image: 'https://source.unsplash.com/random' },
         ]
     },
     {
@@ -33,8 +33,8 @@ export const projects: Project[] = [
         summary: 'Lorem ipsum dolor sit amet',
         image: 'https://source.unsplash.com/random',
         gallery: [
-            'https://source.unsplash.com/random',
-            'https://source.unsplash.com/random',
+            { image: 'https://source.unsplash.com/random' },
+            { image: 'https://source.unsplash.com/random' },
         ]
     },
     {
@@ -43,10 +43,10 @@ export const projects: Project[] = [
         summary: 'Lorem ipsum dolor sit amet',
         image: 'https://source.unsplash.com/random',
         gallery: [
-            'https://source.unsplash.com/random',
-            'https://source.unsplash.com/random',
-            'https://source.unsplash.com/random',
-            'https://source.unsplash.com/random',
+            { image: 'https://source.unsplash.com/random' },
+            { image: 'https://source.unsplash.com/random' },
+            { image: 'https://source.unsplash.com/random' },
+            { image: 'https://source.unsplash.com/random' },
         ]
     },
     {
@@ -55,14 +55,14 @@ export const projects: Project[] = [
         summary: 'Lorem ipsum dolor sit amet',
         image: 'https://source.unsplash.com/random',
         gallery: [
-            'https://source.unsplash.com/random',
-            'https://source.unsplash.com/random',
-            'https://source.unsplash.com/random',
-            'https://source.unsplash.com/random',
-            'https://source.unsplash.com/random',
-            'https://source.unsplash.com/random',
-            'https://source.unsplash.com/random',
-            'https://source.unsplash.com/random',
+            { image: 'https://source.unsplash.com/random' },
+            { image: 'https://source.unsplash.com/random' },
+            { image: 'https://source.unsplash.com/random' },
+            { image: 'https://source.unsplash.com/random' },
+            { image: 'https://source.unsplash.com/random' },
+            { image: 'https://source.unsplash.com/random' },
+            { image: 'https://source.unsplash.com/random' },
+            { image: 'https://source.unsplash.com/random' },
         ]
     },
     {
@@ -71,7 +71,7 @@ export const projects: Project[] = [
         summary: 'Lorem ipsum dolor sit amet',
         image: 'https://source.unsplash.com/random',
         gallery: [
-            'https://source.unsplash.com/random',
+            { image: 'https://source.unsplash.com/random' },
         ]
     },
     {
